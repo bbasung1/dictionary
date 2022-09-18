e56a708f34be9250a10d987e247121c9ce4dccee
bbasung1 <bbasung1@daum.net>
2022-09-18 13:31:19 +0900

    delete wow.txt

--- checktest.js
+++ checktest.js
@@ -1,12 +1,9 @@
-const fs=require('fs');
-const axios=require('axios')
-fs.readFile('./wow.txt','utf-8',function(error,data){
-    console.log("console log")
-    var array=data.toString().split("\n");
-    for(i in array){
-        //console.log(array[i]);
-        axios.get("http://api.dictionaryapi.dev/api/v2/entries/en/"+array[i])
-        .then((data)=>{console.log(data.data[0].word);}).
-        catch(function(err){});
-    }
-})
+const fs = require('fs');
+const axios = require('axios')
+console.log("console log")
+for (i = 2; i < process.argv.length; i++) {
+    //console.log(array[i]);
+    axios.get("http://api.dictionaryapi.dev/api/v2/entries/en/" + process.argv[i])
+        .then((data) => { console.log(data.data[0].word); }).
+        catch(function (err) { });
+}
--- dictionary.cpp
+++ dictionary.cpp
@@ -4,23 +4,24 @@
 #include <stdlib.h>
 #include <fstream>
 #include <algorithm>
+#ifdef _WIN32
+#include <windows.h>
+#endif
 using namespace std;
-ofstream ofs("./wow.txt");
 const int MAX = 8 + 1;
 int fn;
 int N, M,X;
 int arr[MAX];
 bool visited[MAX];
 vector<string>word;
+string cmd ="node checktest.js ";
 void func(int cnt,string k,int pos[],char ltr[])
 {
 	vector<char> tmp;
 	if (cnt == M)
 	{
 		for (int i = 0; i < M; i++)
-			// cout << k[arr[i]-1];
 			tmp.push_back(k[arr[i]-1]);
-		// cout << "n\n";
 		int j=0;
 		for(int i=0;i<fn;i++){
 			tmp.insert(tmp.begin()+pos[i]-1,ltr[j]);
@@ -28,9 +29,6 @@ void func(int cnt,string k,int pos[],char ltr[])
 		}
 		string s(tmp.begin(),tmp.end());
 		word.push_back(s);
-		//cout<<s<<"\n";
-		//ofs<<s;
-		//ofs<<s<<"\n";
 		return;
 	}
 
@@ -46,6 +44,9 @@ void func(int cnt,string k,int pos[],char ltr[])
 
 int main(void)
 {
+	#ifdef _WIN32
+	SetConsoleOutputCP(CP_UTF8);
+	#endif
     string k;
 	cout<<"이미 나온 글자들을 제외하고, 제시 낱말들을 이어 쓰세요: ";
 	cin>>k;
@@ -63,10 +64,13 @@ int main(void)
 	sort(word.begin(),word.end());
 	word.erase(unique(word.begin(),word.end()),word.end());
 	for(auto i:word){
-		cout<<i<<"\n";
-		ofs<<i<<"\n";
+		cmd+=i+" ";
 	}
-	ofs.close();
-	system("node checktest.js > log.txt");
+	cmd+="> log.txt";
+	const char * s =cmd.c_str();
+	cout<<s<<"\n";
+	system(s);
+	//system("node test.js > log.txt vin sag");
+	//cout<<cmd<<"\n";
 	return 0;
 }
